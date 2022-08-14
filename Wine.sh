f2d6e98f7c5e996db4bcd59a155fded2f2004588
Toby <tobiasw763@gmail.com>
2022-08-14 11:06:24 +0200

    Update Wine.sh

--- Wine.sh
+++ Wine.sh
@@ -6,8 +6,7 @@ sudo dpkg --add-architecture i386
 
 sudo wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 
-
-sudo wget -nc -P /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo wget -nc -P /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/impish/winehq-impish.sources
 
 sudo apt update
 
