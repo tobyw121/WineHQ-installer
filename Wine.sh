cd1a3b9cb6845614ef6b8d964ac654904d155544
Toby <tobiasw763@gmail.com>
2022-11-20 18:32:12 +0100

    Update Wine.sh

--- Wine.sh
+++ Wine.sh
@@ -4,9 +4,11 @@ sudo apt update
 
 sudo dpkg --add-architecture i386 
 
-sudo wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo mkdir -pm755 /etc/apt/keyrings
 
-sudo wget -nc -P /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
 
 sudo apt update
 
