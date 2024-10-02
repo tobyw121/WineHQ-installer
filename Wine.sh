19c09a2638ebe120e6cf10d5d8fb34b4fe8defde
Toby <tobiasw763@gmail.com>
2024-10-02 19:44:39 +0200

    Create Wine.sh

---
+++ Wine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo dpkg --add-architecture i386 
+
+sudo mkdir -pm755 /etc/apt/keyrings
+
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org//wine-builds/ubuntu/dists/noble/winehq-noble.sources
+
+sudo apt update
+
+sudo apt-get install -f
+
+sudo apt install --install-recommends winehq-staging
+
+echo "WineHQ Installed"
